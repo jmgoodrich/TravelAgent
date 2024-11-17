6ef7d06714dd607233ad463c2e4a8fa99417f3eb
unknown <jgoodr10@uwyo.edu>
2024-11-17 12:43:19 -0500

    Tool test file

--- test.py
+++ test.py
@@ -1,16 +1,32 @@
 from tools.flightsearch import FlightSearch
+from tools.attractionsearch import AttractionSearch
+from tools.accommodationsearch import AccommodationSearch
 import pandas as pd
 
 flight_csv = pd.read_csv("./database/flights.csv").dropna()[['Flight Number', 'Price', 'DepTime', 'ArrTime', 'ActualElapsedTime','FlightDate','OriginCityName','DestCityName','Distance']]
+attractions_csv = pd.read_csv("./database/attractions.csv").drop(columns=['Latitude', 'Longitude', 'Phone', 'Website'])
+accommodations_csv = pd.read_csv("./database/accommodations.csv").dropna()[['NAME','price','room type','house_rules', 'minimum nights', 'maximum occupancy', 'city']]
 
 flight_tool = FlightSearch()
-
-
-result = flight_tool.run({
+result_flights = flight_tool.run({
     "data": flight_csv,
     "origin": "Sarasota",
     "dest": "Chicago",
     "date": "2022-03-22"
 })
 
-print(result)
+attractions_tool = AttractionSearch()
+result_attr = attractions_tool.run({
+    "data": attractions_csv,
+    "city": "Chicago"
+})
+
+accommodations_tool = AccommodationSearch()
+result_accom = accommodations_tool.run({
+    "data": accommodations_csv,
+    "city": "Chicago"
+})
+
+print(result_flights)
+print(result_attr)
+print(result_accom)
